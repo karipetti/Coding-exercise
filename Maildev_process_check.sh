462e240c9d58fa6bed0adf09075bf44b0cfe2610
karipetti <44439382+karipetti@users.noreply.github.com>
2018-10-25 04:43:00 +0530

    Create Maildev_process_check.sh

---
+++ Maildev_process_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#To Check if maildev process is running or not
+ps -eaf | grep [m]aildev >/dev/null
+if [ $? -ne 0 ]
+then
+sudo start maildev
+else
+#sending mail if the process is running
+mailx -s "maildev process is running" kpi.nivas@gmail.com
+fi
+exit 0
+
+
